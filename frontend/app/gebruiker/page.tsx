0c7c5a6017d8928e2e53fc9e3ea4f3b219cb44ab
Spronu <yildizyahya.38@gmail.com>
2023-10-13 21:12:53 +0200

    Frontend Profielgegevens
    Concept afgemaakt nog niet helemaal af.

---
+++ frontend/app/gebruiker/page.tsx
@@ -0,0 +1,45 @@
+import Container from "../components/Container";
+import styles from "./page.module.css";
+
+const Page = () => {
+  return (
+    <Container title="Profiel">
+      <div className={styles.parentContainer}>
+        <img
+          className={styles.circularProfilePhoto}
+          src="https://randomuser.me/api/portraits/men/88.jpg"
+          alt="Profile"
+        />
+        <div className={styles.userInfo}>
+          <table>
+            <thead>
+              <tr>
+                <th>Profielgegevens</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Naam:</td>
+              </tr>
+              <tr>
+                <td>Leeftijd:</td>
+              </tr>
+              <tr>
+                <td>Email:</td>
+              </tr>
+              <tr>
+                <td>Bio:</td>{" "}
+              </tr>
+              <tr>
+                <td>Punten:</td>
+              </tr>
+            </tbody>
+          </table>
+          <button className={styles.saveButton}>Opslaan</button>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Page;
