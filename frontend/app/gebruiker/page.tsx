7b675cf8398539f35f7b5ee445ea224b50ac5d3d
Spronu <yildizyahya.38@gmail.com>
2023-10-15 21:01:11 +0200

    Gebruikersprofiel
    Mobiel compatible gemaakt en dummy data gevoegd CSS aangepast en TSX.

--- frontend/app/gebruiker/page.tsx
+++ frontend/app/gebruiker/page.tsx
@@ -1,41 +1,51 @@
+"use client";
+import { useState } from "react";
 import Container from "../components/Container";
 import styles from "./page.module.css";
 
 const Page = () => {
+  const [buttonText] = useState("Opslaan");
+
+  const userData = {
+    name: "John Doe",
+    age: 30,
+    email: "johndoe@example.com",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    points: 100,
+  };
+
   return (
     <Container title="Profiel">
       <div className={styles.parentContainer}>
-        <img
-          className={styles.circularProfilePhoto}
-          src="https://randomuser.me/api/portraits/men/88.jpg"
-          alt="Profile"
-        />
+        <div className={styles.circularProfilePhotoContainer}>
+          <img
+            src="https://randomuser.me/api/portraits/men/88.jpg"
+            alt="Profile"
+            className={styles.circularProfilePhoto}
+          />
+        </div>
         <div className={styles.userInfo}>
-          <table>
-            <thead>
-              <tr>
-                <th>Profielgegevens</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Naam:</td>
-              </tr>
-              <tr>
-                <td>Leeftijd:</td>
-              </tr>
-              <tr>
-                <td>Email:</td>
-              </tr>
-              <tr>
-                <td>Bio:</td>
-              </tr>
-              <tr>
-                <td>Punten:</td>
-              </tr>
-            </tbody>
-          </table>
-          <button className={styles.saveButton}>Opslaan</button>
+          <div>
+            <label>Naam:</label>
+            <p>{userData.name}</p>
+          </div>
+          <div>
+            <label>Leeftijd:</label>
+            <p>{userData.age}</p>
+          </div>
+          <div>
+            <label>Email:</label>
+            <p>{userData.email}</p>
+          </div>
+          <div>
+            <label>Bio:</label>
+            <p>{userData.bio}</p>
+          </div>
+          <div>
+            <label>Punten:</label>
+            <p>{userData.points}</p>
+          </div>
+          <button className={styles.saveButton}>{buttonText}</button>
         </div>
       </div>
     </Container>
