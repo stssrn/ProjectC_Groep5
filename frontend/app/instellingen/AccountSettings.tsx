96d4df96cfe6962fc93e478daa628b1726298808
stssrn <stssrn@pm.me>
2023-11-16 11:49:39 +0100

    Instellingen (#11)
    Styled the Save Button in AccountSettings.TSX and converted the files AccountSettings and NotificationSettings to TSX files instead of JSX.
    
    Co-authored-by: Fm&hm8S4M3ocyo$z <yildizyahya.38@gmail.com>

--- frontend/app/instellingen/AccountSettings.tsx
+++ frontend/app/instellingen/AccountSettings.tsx
@@ -1,6 +1,37 @@
 "use client";
 import React, { useState } from "react";
 import { SettingOption, Switch } from "../components/SettingsComponents";
+import styled from "@emotion/styled";
+
+const StyledButton = styled.button`
+  padding: 5px 10px;
+  font-size: 16px;
+  font-family: Arial, sans-serif;
+  background-color: #9b2c50;
+  color: white;
+  border: 2px solid #9b2c50;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s, border-color 0.3s;
+  margin-left: 10px;
+
+  &:hover {
+    background-color: #7c203e;
+    border-color: #7c203e;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px rgba(0, 123, 255, 0.5);
+  }
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+    padding: 4px 8px;
+    margin-left: 0px;
+    margin-top: 10px;
+  }
+`;
 
 const AccountSettings = () => {
   const [email, setEmail] = useState(
@@ -12,7 +43,9 @@ const AccountSettings = () => {
       localStorage.getItem("twoFactorEnabled") === "true"
   );
 
-  const handleEmailChange = (event) => {
+  const handleEmailChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
     if (typeof window !== "undefined") {
       setEmail(event.target.value);
     }
@@ -28,7 +61,7 @@ const AccountSettings = () => {
     const newValue = !isTwoFactorEnabled;
     setTwoFactorEnabled(newValue);
     if (typeof window !== "undefined") {
-      localStorage.setItem("twoFactorEnabled", newValue);
+      localStorage.setItem("twoFactorEnabled", newValue.toString());
     }
   };
 
@@ -37,7 +70,9 @@ const AccountSettings = () => {
       <SettingOption>
         <span>Email</span>
         <input type="email" value={email} onChange={handleEmailChange} />
-        <button onClick={handleEmailSave}>Save Email</button>
+        <StyledButton onClick={handleEmailSave}>
+          <span>Save Email</span>
+        </StyledButton>
       </SettingOption>
       <SettingOption>
         <span>Enable Two-Factor Authentication</span>
