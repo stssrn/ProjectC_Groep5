38e3c1801031f3bc424021766e536dadca09f300
Undertaker <124693454+Undertaker2104@users.noreply.github.com>
2023-11-23 15:48:58 +0100

    Verder en skip knop en text align

--- frontend/app/ggzinfo/page.tsx
+++ frontend/app/ggzinfo/page.tsx
@@ -1,42 +1,48 @@
 "use client";
 import styles from "./page.module.css";
-import React from "react";
-import image from "./image1.svg";
+import React, { useState } from "react";
+import image from "./classroom.svg";
 import Image from "next/image";
+import Link from "next/link";
 
-
-
-const DisplayInfo = () => {
-
+const Display = () => {
     const ggzInfo: string[] = [
-        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. \
-         Aenean commodo ligula eget dolor.Aenean massa.Cum sociis ",
-
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis ",
         "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, \
-         sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
-
+        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
         "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, \
-         sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.\
-         Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
-
-        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. \
-         Aenean commodo ligula eget dolor.Aenean massa.Cum sociis.",
+        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. \
+        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis.",
     ];
+
+    const [currentInfoIndex, setCurrentInfoIndex] = useState(0);
+
+    const handleContinue = () => {
+        // Increment the index and loop back to the beginning if at the end
+        setCurrentInfoIndex((prevIndex) => (prevIndex + 1) % ggzInfo.length);
+    };
+
     return (
         <div>
-            <div className={styles.middle}>
+            <div>
+                <Link href="/login" className={styles.skipButton}>
+                    Skip
+                </Link>
+            </div>
+            <div className={styles.center}>
                 <Image className={styles.image} src={image} alt="" />
             </div>
-            <br></br><br></br><h1 className={styles.ggzTitel}>Antes informatie</h1>
             <div className={styles.ggzText}>
-                <p>{ggzInfo[0]}</p>
+                <p>{ggzInfo[currentInfoIndex]}</p>
             </div>
-            
+            <div>
+                <button className={styles.continueButton} onClick={handleContinue}>
+                    Verder
+                </button>
+            </div>
         </div>
-
-        
-    )
-
+    );
 };
 
-export default DisplayInfo;
+export default Display;
