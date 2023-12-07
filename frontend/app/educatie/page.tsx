821200d94160d27538c1c7b64410677bfded0255
Undertaker <124693454+Undertaker2104@users.noreply.github.com>
2023-12-07 09:04:23 +0100

    Revert "Onnodige comments weggehaald"
    This reverts commit f0b4c043eebfef47949a028050420946903699ec.

--- frontend/app/educatie/page.tsx
+++ frontend/app/educatie/page.tsx
@@ -46,15 +46,18 @@ const Page: React.FC = () => {
     };
 
     useEffect(() => {
+        // Define a function to update the window width
         const updateWindowWidth = () => {
             setWindowWidth(window.innerWidth);
         };
 
+        // Add an event listener for window resize
         window.addEventListener("resize", updateWindowWidth);
 
+        // Call the function to set the initial window width
         updateWindowWidth();
 
-
+        // Remove the event listener when the component is unmounted
         return () => {
             window.removeEventListener("resize", updateWindowWidth);
         };
@@ -68,8 +71,8 @@ const Page: React.FC = () => {
                     <Image
                         src={hamburgerMenu}
                         alt=""
-                        width={50}
-                        height={50}
+                        width={50} // Adjust width as needed
+                        height={50} // Adjust height as needed
                     />
                 </div>
                 {(isMenuOpen || windowWidth >= 451) && (
