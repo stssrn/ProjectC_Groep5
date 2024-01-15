df578fbf69b752eb25a88768f6dfab671f69e5b9
Fm&hm8S4M3ocyo$z <yildizyahya.38@gmail.com>
2024-01-15 15:14:37 +0100

    export erbij gezet bij authoptions

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import { compare } from "bcrypt";
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-const authOptions: NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/login",
